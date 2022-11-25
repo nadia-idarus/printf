c72fc2f2f5c95a83ebdaf3351f5966dccefe7e70
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2022-11-25 08:09:01 -0800

    Main.c

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x11.C - printf team project 
+0x11.C- printf Aim of this project is to collaborate and show proactiveness in coding
--- main.c
+++ main.c
@@ -1,40 +1,43 @@
+#include <limits.h>
 #include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
 
-int _printf(const char *format, ...)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-	    int count = 0;
-	     va_list args;
-	     va_start(args, format);
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
 
-	       // handle c specifier
-	          if (*format == 'c')
-		  {
-			  char c = va_arg(args, int);
-			     putchar(c);
-			       count++;
-		  }
-		    // handle s specifier
-		     else if (*format == 's')
-		     {
-			     char *s = va_arg(args, char*);
-			     int i;
-			     for (i = 0; s[i] != '\0'; i++)
-			     {
-				     putchar(s[i]);
-				      count++;
-			     }
-		    }
-		     // handle % specifier
-		      else if (*format == '%')
-		      {
-			      putchar('%');
-			      count++;
-		      }
-		        else
-			{
-				 // do nothing
-			}
-			va_end(args);
-			 return count;
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned:[%u]\n", ui);
+	printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Address:[%p]\n", addr);
+	printf("Address:[%p]\n", addr);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
+	return (0);
 }
